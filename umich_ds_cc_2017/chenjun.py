5ea4fed959a716a2b4daa32150fc3d6c7a1f51e1
Chenjun Yang <yangchenj@gmail.com>
2017-09-17 14:50:44 -0400

    update version

--- umich_ds_cc_2017/chenjun.py
+++ umich_ds_cc_2017/chenjun.py
@@ -10,29 +10,34 @@ from sklearn.neighbors import KNeighborsClassifier
 dict = {}
 dict_rev = {}
 
-def data_preprocess(directory):
-	data,feature,name,label,label_name,freq,length = [],[],[],[],[],[],5000
+def data_preprocess(directory, length=6000, test = False):
+	data,feature,name,label,label_name,freq = [],[],[],[],[],[]
 	# parse input
-	for file in glob.glob(directory+"*.wav"):
-		freq.append(wavfile.read(file)[0])
-		# get the name of each label
-		label_name.append(str(file)[13:-7])
-		# get the name of each audio file
-		name.append(str(file)[13:])
+	if test == False:
+		for file in glob.glob(directory+"*.wav"):
+			freq.append(wavfile.read(file)[0])
+			# get the name of each label
+			label_name.append(str(file)[13:-7])
+			# get the name of each audio file
+			name.append(str(file)[13:])
+			# convert labels into number
+		for l in label_name:
+			if l in dict:
+				label.append(dict[l])
+			else:
+				dict[l] = len(label)
+				dict_rev[dict[l]] = l 
+				label.append(dict[l])
+	else:
+		for file in glob.glob(directory+"*.wav"):
+			freq.append(wavfile.read(file)[0])
+			# get the name of each audio file
+			name.append(str(file)[12:])
 
 	for file in glob.glob(directory+"*.wav"):
 		# read audio data into numpy.array
 		data.append(wavfile.read(file)[1][:length])
-
-	# convert labels into number
-	for l in label_name:
-		if l in dict:
-			label.append(dict[l])
-		else:
-			dict[l] = len(label)
-			dict_rev[dict[l]] = l 
-			label.append(dict[l])
-
+	
 	# convert list into flaot numpy.array
 	data = np.asarray(data)
 	data = data.astype(float)
@@ -46,35 +51,11 @@ def data_preprocess(directory):
 
 	feature = np.asarray(feature)
 	label = np.asarray(label)
-	return [feature, name, label]
-
-def data_preprocess2(directory):
-	data,feature,name,freq,length = [],[],[],[],5000
-
-	# parse input
-	for file in glob.glob(directory+"*.wav"):
-		freq.append(wavfile.read(file)[0])
-		# get the name of each audio file
-		name.append(str(file)[12:])
-
-	for file in glob.glob(directory+"*.wav"):
-		# read audio data into numpy.array
-		data.append(wavfile.read(file)[1][:length])
-
-	# convert list into flaot numpy.array
-	data = np.asarray(data)
-	data = data.astype(float)
 	
-	# normalize data vector	
-	for i in range(0,len(feature)):
-		data[i] = data[i]/np.linalg.norm(data[i])
-
-	#  generate feature vector
-	for i in range(0,len(data)):
-		feature.append(mfcc(data[i],freq[i]).reshape(-1))
-
-	feature = np.asarray(feature)
-	return [feature, name]
+	if test:
+		return [feature, name]
+	else:
+		return [feature, name, label]
 
 def train(data, label):
 	clf = KNeighborsClassifier(10)
@@ -89,7 +70,6 @@ def test(clf,data,label=None):
 	if label != None:
 		correct = 0;
 		for i in range(0,len(label)):
-			print(dict[output[i]],label[i])
 			if label[i] == dict[output[i]]:
 				correct +=1
 		print("accuracy is {} \n".format(float(correct)/len(output)))
@@ -103,7 +83,7 @@ def format_output(name, pred):
 
 if __name__ == '__main__':
 	[train_data, train_name, train_label] = data_preprocess("./train_data/")
-	[test_data, test_name] = data_preprocess2("./test_data/")
+	[test_data, test_name] = data_preprocess("./test_data/",test=True)
 
 	clf = train(train_data,train_label)
 	pred = test(clf,test_data)
